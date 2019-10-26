60219e98b1f41a411d67b7605bf09880ab1596e2
Mercur.Codes <Raphael.Kunz@uni-ulm.de>
2019-10-26 20:34:29 +0200

    update

---
+++ Webframe/Login_java.js
@@ -0,0 +1,13 @@
+<script>
+		function logIn() {
+					
+				document.getElementById("Password").setAttribute("style", "border-color: red");
+				tries = 1;	
+				
+				document.getElementById("Password").value = ""			
+			
+				document.getElementById("Button").setAttribute("onClick", "window.location='Schulauswahl.html'");
+			
+			
+		}
+	</script>// JavaScript Document
